98ad6acb834fa6a2034b6d86f98b162190e9988b
kurra-avinash <kavinash.k16@gmail.com>
2026-05-18 03:29:24 +0530

    New class added

---
+++ src/test/java/TestRunner/demon.java
@@ -0,0 +1,17 @@
+package TestRunner;
+
+public class demon {
+	public void add()
+	{
+	System.out.println("Add");
+	}
+	public void div()
+	{
+		System.out.println("div");
+	}
+	public void mul()
+	{
+		System.out.println("mul");
+	}
+
+}
